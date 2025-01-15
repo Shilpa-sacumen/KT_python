0ac7fd14cccef4b700d49e14990b48dbb8adab5e
Shilpa-sacumen <shilpa.veeranagappa@sacumen.com>
2025-01-15 18:03:26 +0530

    added unit testing1

--- tests/test_slack.py
+++ tests/test_slack.py
@@ -20,79 +20,79 @@ def test_conversation_list_fail():
     response=user_1.list_conversation(url)
     assert response.status_code==200
 
-# def test_list_conversation_error():
-#     """to test list_conversations 
-#     """
-#     url='https://slack.com/apl/conversations.list'
-#     with pytest.raises(HTTPError):
-#         user_1.list_conversation(url)
+def test_list_conversation_error():
+    """to test list_conversations 
+    """
+    url='https://slack.com/apl/conversations.list'
+    with pytest.raises(HTTPError):
+        user_1.list_conversation(url)
 
-# def test_list_conversation_negative():
-#     """to test 
-#     """
-#     url1='https://sliack.com/api/users.list'
-#     with pytest.raises(HTTPError):
-#         user_1.list_user(url1)
-
-
+def test_list_conversation_negative():
+    """to test 
+    """
+    url1='https://sliack.com/api/users.list'
+    with pytest.raises(HTTPError):
+        user_1.list_user(url1)
 
 
-# def test_user_list_fail():
-#     """ to test list_conversation api """
-#     # params={"limit":2,
+
+
+def test_user_list_fail():
+    """ to test list_conversation api """
+    # params={"limit":2,
         
-#     #     }
+    #     }
     
 
-#     response=user_1.list_user(url1)
-#     assert response.status_code==200
+    response=user_1.list_user(url1)
+    assert response.status_code==200
 
 
-# def test_conversation_list_unauth():
-#     """ to test list_conversation api """
-#     # params={"limit":2,
+def test_conversation_list_unauth():
+    """ to test list_conversation api """
+    # params={"limit":2,
         
-#     #     }
+    #     }
     
-#     url='https://slack.com/apl/conversations.list'
-#     response=user_1.list_conversation(url)
-#     assert response.status_code==404
+    url='https://slack.com/apl/conversations.list'
+    response=user_1.list_conversation(url)
+    assert response.status_code==404
 
 
-# def test_list_converstion_mocker(mocker:Any):
-#     """_summary_
+def test_list_converstion_mocker(mocker:Any):
+    """_summary_
 
-#     Args:
-#         mocker (Any): _description_
-#     """
+    Args:
+        mocker (Any): _description_
+    """
     
-#     mocker.patch("requests.get",mock_list_conversation)
-#     response=user_1.list_conversation(url="test_url")
-#     data=response.json()
-#     assert response.status_code==200
-#     assert data['ok']=='true'
+    mocker.patch("requests.get",mock_list_conversation)
+    response=user_1.list_conversation(url="test_url")
+    data=response.json()
+    assert response.status_code==200
+    assert data['ok']=='true'
 
 
-# def test_list_converstion_mocker(mocker:Any):
-#     """_summary_
+def test_list_converstion_mocker(mocker:Any):
+    """_summary_
 
-#     Args:
-#         mocker (Any): _description_
-#     """
+    Args:
+        mocker (Any): _description_
+    """
     
-#     mocker.patch("requests.get",mock_list_conversation_fail)
-#     response=user_1.list_conversation(url="test_url")
-#     data=response.json()
-#     assert response.status_code==404
-#     assert data['error']=="Invalid authorization"
+    mocker.patch("requests.get",mock_list_conversation_fail)
+    response=user_1.list_conversation(url="test_url")
+    data=response.json()
+    assert response.status_code==404
+    assert data['error']=="Invalid authorization"
 
-# @pytest.mark.vcr()
-# def test_list_conversation_vcr():
-#     """_summary_
-#     """
+@pytest.mark.vcr()
+def test_list_conversation_vcr():
+    """_summary_
+    """
 
-#     response=user_1.list_conversation(url)
-#     assert response.status_code==200
+    response=user_1.list_conversation(url)
+    assert response.status_code==200
 
 @pytest.mark.vcr()
 def test_list_user_vcr():
